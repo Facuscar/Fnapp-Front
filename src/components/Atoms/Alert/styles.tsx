c12587597fc6ffffd4de2347d0997954507838d9
Facuscar <facuscardillo@gmail.com>
2023-04-16 00:02:28 -0300

    Install polished

--- src/components/Atoms/Alert/styles.tsx
+++ src/components/Atoms/Alert/styles.tsx
@@ -1,6 +1,7 @@
-import styled, { lighten } from "styled-components";
+import styled from "styled-components";
+import { lighten } from "polished";
 
 export const AlertWrapper = styled.div`
   width: 100%;
-  background-color: ${({ theme }) => lighten(0.2, theme.colors.special.red)};
+  background-color: ${({ theme }) => lighten(0.1, theme.colors.special.red)};
 `
