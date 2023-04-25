d666fc103b5bc98cd0d9bc53e93115d5ab724483
Facuscar <facuscardillo@gmail.com>
2023-04-25 16:42:27 -0300

    Validate if passwords match when registering

--- src/components/Header/LogIn/RegisterForm/index.tsx
+++ src/components/Header/LogIn/RegisterForm/index.tsx
@@ -1,9 +1,9 @@
-import Input from '@fnapp/components/Atoms/Form/Input';
+import { useState, useRef } from 'react';
 
+import Input from '@fnapp/components/Atoms/Form/Input';
 import { validatePassword } from '@fnapp/utils/validatePassword';
 
 import * as S from './styles';
-import { useState } from 'react';
 
 interface RegisterFormProps {
   email: string
@@ -11,23 +11,43 @@ interface RegisterFormProps {
 
 const RegisterForm: React.FC<RegisterFormProps> = ({ email }) => {
   const [passwordError, setPasswordError] = useState<boolean>(false);
+  const [secondPasswordError, setSecondPasswordError] = useState<boolean>(false);
+
+  const nameRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
 
   const validatePasswordInput = (password: string) => {
     setPasswordError(!validatePassword(password));
   }
 
+  const comparePasswords = (secondPassword: string) => {
+    if (secondPassword !== passwordRef.current?.value) {
+      setSecondPasswordError(true);
+      return;
+    }
+
+    setSecondPasswordError(false);
+  };
+
   return (
     <S.Form>
       <Input type='email' defaultValue={email} name='Email' disabled />
-      <Input type='text' name='Name' placeholder='Your name' />
+      <Input type='text' name='Name' placeholder='Your name' ref={nameRef} />
       <Input
         type='password'
         name='Password'
         onChange={e => { validatePasswordInput(e.currentTarget.value) }}
         errorMessage='At least one number, one letter, and one special character'
         hasError={passwordError}
+        ref={passwordRef}
       />
-      <Input type='password' name='Confirm your password' />
+      <Input
+        type='password'
+        name='Confirm your password'
+        hasError={secondPasswordError}
+        errorMessage='Passwords do not match'
+        onChange={e => { comparePasswords(e.currentTarget.value) }}
+      />
       <S.RegisterButton>
         Register
       </S.RegisterButton>
