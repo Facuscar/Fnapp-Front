db8e8cd29d26d9db27ae4302d0fbee54c1b0660b
Facuscar <facuscardillo@gmail.com>
2023-04-28 21:53:25 -0300

    Create confirm account page with the token

---
+++ src/app/confirm/[token]/page.tsx
@@ -0,0 +1,18 @@
+import { Header } from '@fnapp/components/Header/Header';
+
+interface PageProps {
+  params: { token: string }
+}
+
+const Page: React.FC<PageProps> = ({ params }) => {
+  const { token } = params;
+
+  return (
+    <>
+      <Header />
+      Confirmation page {token}
+    </>
+  );
+}
+
+export default Page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import { Header } from "@fnapp/components/Header/Header"
+import { Header } from '@fnapp/components/Header/Header'
 
-export default function Home() {
+export default function Home () {
   return (
     <>
       <Header />
       <main>
-      
+
       </main>
     </>
   )
