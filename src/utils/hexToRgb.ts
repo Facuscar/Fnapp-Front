9207cc691538da65a4bb0e893044be5e49f5305f
Facuscar <facuscardillo@gmail.com>
2023-05-10 00:02:56 -0300

    Fix unused util function

--- src/utils/hexToRgb.ts
+++
@@ -1,9 +0,0 @@
-export const hexToRgb = (hex: string) => {
-  hex = hex.replace('#', '');
-
-  var r = parseInt(hex.substring(0, 2), 16);
-  var g = parseInt(hex.substring(2, 4), 16);
-  var b = parseInt(hex.substring(4, 6), 16);
-
-  return {r, g, b};
-};
