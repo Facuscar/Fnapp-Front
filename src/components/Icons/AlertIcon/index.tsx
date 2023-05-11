c8e339602efbe3b880451dd77f994d8b44fdf328
Facuscar <facuscardillo@gmail.com>
2023-05-11 00:31:41 -0300

    Remove unnecesary styles files

--- src/components/Icons/AlertIcon/index.tsx
+++ src/components/Icons/AlertIcon/index.tsx
@@ -1,7 +1,10 @@
-import * as S from './styles';
-
 const Alert: React.FC = (props) => (
-  <S.Alert {...props} />
+  <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round" {...props}>
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+    <circle cx="12" cy="12" r="9" />
+    <line x1="12" y1="8" x2="12" y2="12" />
+    <line x1="12" y1="16" x2="12.01" y2="16" />
+  </svg>
 );
 
 export default Alert;
