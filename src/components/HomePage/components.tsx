6c8a47e2b02a75d847e660287117d6c5fdeac804
Facuscar <facuscardillo@gmail.com>
2023-05-24 19:59:10 -0300

    First steps with the homepage component

--- src/components/HomePage/components.tsx
+++ src/components/HomePage/components.tsx
@@ -5,3 +5,7 @@ import styles from './styles.module.scss';
 export const HomePageWrapper: React.FC<DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>> = (props) => (
   <div className={styles.home_page_wrapper} {...props} />
 );
+
+export const Title: React.FC<DetailedHTMLProps<HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>> = (props) => (
+  <h1 className={styles.title} {...props} />
+);
--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,10 +1,8 @@
-import { HomePageWrapper } from './components';
-
-import styles from './styles.module.scss';
+import { HomePageWrapper, Title } from './components';
 
 const HomePage: React.FC = () => (
   <HomePageWrapper>
-      <h1 className={styles.h1}>Homepage for unique<span>users</span></h1>
+    <Title>All your expenses, in one <span>app</span></Title>
   </HomePageWrapper>
 );
 
