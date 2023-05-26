16740dbe9ea98af3e29bef917ff08f945668c0ef
Facuscar <facuscardillo@gmail.com>
2023-05-25 23:24:26 -0300

    First steps with the homepage

--- src/components/HomePage/components.tsx
+++ src/components/HomePage/components.tsx
@@ -2,10 +2,22 @@ import { type DetailedHTMLProps, type HTMLAttributes } from 'react';
 
 import styles from './styles.module.scss';
 
-export const HomePageWrapper: React.FC<DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>> = (props) => (
-  <div className={styles.home_page_wrapper} {...props} />
+export const HomePageWrapper: React.FC<DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>> = (props) => (
+  <section className={styles.home_page_wrapper} {...props} />
 );
 
 export const Title: React.FC<DetailedHTMLProps<HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>> = (props) => (
   <h1 className={styles.title} {...props} />
+);
+
+export const MainWrapper: React.FC<DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>> = (props) => (
+  <div className={styles.main_wrapper} {...props} />
+);
+
+export const TextWrapper: React.FC<DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>> = (props) => (
+  <div {...props} />
+);
+
+export const SubTitle: React.FC<DetailedHTMLProps<HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>> = (props) => (
+  <div {...props} className={styles.subtitle} />
 );
--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,8 +1,18 @@
-import { HomePageWrapper, Title } from './components';
+import Image from 'next/image';
+
+import phoneImage from '@public/assets/home_image.png';
+
+import { HomePageWrapper, Title, MainWrapper, TextWrapper, SubTitle } from './components';
 
 const HomePage: React.FC = () => (
   <HomePageWrapper>
-    <Title>All your expenses, in one <span>app</span></Title>
+    <MainWrapper>
+      <TextWrapper>
+        <Title>All your expenses, in one <span>app</span></Title>
+        <SubTitle>Track, Analyze, and Master Your Finances. Take control of your expenses, gain insights, and achieve financial freedom with ease</SubTitle>
+      </TextWrapper>
+      <Image src={phoneImage} height={800} width={400} alt='Smartphone with different incomes and outcomes of money' />
+    </MainWrapper>
   </HomePageWrapper>
 );
 
