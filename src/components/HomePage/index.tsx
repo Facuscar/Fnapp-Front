5cdd2dbac7bb8fccef6a189ef3d02752a5eca463
Facuscar <facuscardillo@gmail.com>
2023-05-25 23:51:31 -0300

    Small adjustments

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -11,7 +11,7 @@ const HomePage: React.FC = () => (
       <TextWrapper>
         <Title>All your expenses, in one <span>app</span></Title>
         <SubTitle>Track, Analyze, and Master Your Finances. Take control of your expenses, gain insights, and achieve financial freedom with ease.</SubTitle>
-        <Image src={downloadApp} height={220} width={250} alt='Download on Google Play, Download on App Store' />
+        <Image src={downloadApp} height={220} width={270} alt='Download on Google Play, Download on App Store' />
       </TextWrapper>
       <Image src={phoneImage} height={800} width={400} alt='Smartphone with different incomes and outcomes of money' />
     </MainWrapper>
