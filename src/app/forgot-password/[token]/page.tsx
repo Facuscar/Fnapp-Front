7e7d9d7bf945d1f7fd4b7c03517e2976f4627676
Facuscar <facuscardillo@gmail.com>
2023-05-31 23:16:02 -0300

    Validate if token is valid when recuperating password

---
+++ src/app/forgot-password/[token]/page.tsx
@@ -0,0 +1,42 @@
+import axios from 'axios';
+
+import { Header } from '@fnapp/components/Header/Header';
+import Alert from '@fnapp/components/Atoms/Alert';
+
+interface PageProps {
+  params: { token: string }
+}
+
+/* @ts-expect-error Async Server Component */
+const Page: React.FC<PageProps> = async ({ params }) => {
+  const { token } = params;
+
+  const validateToken = async () => {
+    try {
+      await axios.get<{ msg: string }>(`${process.env.NEXT_PUBLIC_API_USERS_URL}/forgot-password/${token}`);
+    } catch (error: any) {
+      if (error.response !== undefined) {
+        return {
+          msg: error.response.data.msg as string
+        };
+      }
+
+      return {
+        msg: 'Oops.. there was an error..'
+      }
+    }
+  }
+
+  const data = await validateToken();
+
+  const msg = data?.msg;
+
+  return (
+    <>
+      <Header />
+      { msg !== undefined && <Alert msg={msg} error />}
+    </>
+  );
+};
+
+export default Page;
