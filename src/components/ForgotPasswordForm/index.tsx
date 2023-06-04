2d510204438e70a8c389d957d94b54303088645d
Facuscar <facuscardillo@gmail.com>
2023-06-03 22:44:52 -0300

    Add validation for the recover password form

--- src/components/ForgotPasswordForm/index.tsx
+++ src/components/ForgotPasswordForm/index.tsx
@@ -1,23 +1,75 @@
 'use client';
 
-import { useRef } from 'react';
+import { useRef, useState, type SyntheticEvent } from 'react';
 
 import Input from '@fnapp/components/Atoms/Form/Input';
+import { validatePassword } from '@fnapp/utils/validatePassword';
 
 import { Form, Title, Wrapper, SubmitButton } from './components';
 
 const ForgotPasswordForm: React.FC = () => {
-  const passwordRef = useRef<HTMLInputElement>(null)
-  const secondPasswordRef = useRef<HTMLInputElement>(null)
+  const [passwordError, setPasswordError] = useState<boolean>(false);
+  const [secondPasswordError, setSecondPasswordError] = useState<boolean>(false);
+
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const secondPasswordRef = useRef<HTMLInputElement>(null);
+
+  const updatePassword = () => {
+    const password = passwordRef.current?.value;
+
+    if (password === undefined) return;
+  }
+
+  const validatePasswordInput = (password: string) => {
+    setPasswordError(!validatePassword(password));
+    if ((secondPasswordRef.current?.value) !== '') {
+      comparePasswords()
+    }
+  }
+
+  const comparePasswords = () => {
+    if (secondPasswordRef.current?.value !== passwordRef.current?.value) {
+      setSecondPasswordError(true);
+      return;
+    }
+
+    setSecondPasswordError(false);
+  };
+
+  const validateForm = () => {
+    return !(passwordError || secondPasswordError);
+  }
+
+  const handleSubmit = (e: SyntheticEvent) => {
+    e.preventDefault();
+    if (!validateForm()) return;
+
+    updatePassword();
+  }
 
   return (
     <Wrapper>
       <Title>
         Recover your password
       </Title>
-      <Form>
-        <Input name='New password' type='password' ref={passwordRef} autoComplete='new-password' />
-        <Input name='Repeat your password' type='password' ref={secondPasswordRef} />
+      <Form onSubmit={handleSubmit}>
+        <Input
+          name='New password'
+          type='password'
+          ref={passwordRef}
+          autoComplete='new-password'
+          onChange={e => { validatePasswordInput(e.currentTarget.value) }}
+          hasError={passwordError}
+          errorMessage='At least one number, one letter, and one special character'
+        />
+        <Input
+          name='Repeat your password'
+          type='password'
+          ref={secondPasswordRef}
+          onChange={comparePasswords}
+          hasError={secondPasswordError}
+          errorMessage='Passwords do not match'
+        />
         <SubmitButton>Reset password</SubmitButton>
       </Form>
     </Wrapper>
